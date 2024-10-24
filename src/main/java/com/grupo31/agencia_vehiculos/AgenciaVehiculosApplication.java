355c36268e084d638ce7cf41029bc29cdab7d829
francososa67 <sosa44192872@gmail.com>
2024-10-24 18:10:42 -0300

    creandoProyecto

---
+++ src/main/java/com/grupo31/agencia_vehiculos/AgenciaVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.grupo31.agencia_vehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaVehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo31/agencia_vehiculos/AgenciaVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo31.agencia_vehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
